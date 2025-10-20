6f8d2cd438c691f5018712593260ebb46321e9d9
Mohan Kumar <63906309+MohanKumarD-tech@users.noreply.github.com>
2025-10-20 10:52:14 +0530

    Time: 766 ms (7.11%), Space: 45.5 MB (5.07%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        char[] s1Sort = s1.toCharArray();
+        Arrays.sort(s1Sort);
+
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            String subStr = s2.substring(i, i + s1.length());
+            char[] subArr = subStr.toCharArray();
+            Arrays.sort(subArr);
+
+            if (Arrays.equals(s1Sort, subArr)) {
+                return true;
+            }
+        }
+
+        return false;
+  }
+}
